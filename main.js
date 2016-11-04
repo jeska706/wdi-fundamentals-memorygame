bcb050ea6e9100d784353d07f0453bbe9bf0eb76
jeska706 <jessicabeardsley@hotmail.com>
2016-11-03 22:45:08 -0600

    Setting up card images, still needs work

--- main.js
+++ main.js
@@ -1,51 +1,45 @@
-
-//4 ) Got an element with the id of game-board and set it to a variable
+//name element with the id of game-board and set it to a variable
 var gameBoard = document.getElementById('gameBoard');
 
-//setting card variables
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "King";
-var cardFour = "Queen";
-
-//setting for loop
-for (var i = 0; i < 4; i++){
-		var cards = document.createElement("div");
-		console.log(cards);
-		cards.classList.add("card");
-		gameBoard.appendChild(cards);
-};
-
-//declaring variable whose value is an array
+//declaring the cards/images in an array
 var cardNumber = ["Queen", "King", "King", "Queen"];
+var image = ["<img src='images/queen-card.png'>", "<img src='images/king-card.png'>", "<img src='images/king-card.png'>", "<img src='images/queen-card.png'>"];
 
-//declaring a variable whose value is an empty array
-var cardsInPlay = [];{
- cardNumber
-}
+//declaring the cardsInPlay to an empty array
+var cardsInPlay = [];
+
+//delacring createBoard function, setting card divs, images, and eventlistener
+var createBoard = function(){
 for (var i = 0; i < cardNumber.length; i++){
-}
+	var cards = document.createElement("div");
+	// console.log(cards);
+	cards.classList.add("card");
+	gameBoard.appendChild(cards);
+	cards.setAttribute('data-card', cardNumber[i]);
+	cards.addEventListener("click", isTwoCards);
+	cards.innerHTML = (image[i]);
 
-//function that will trigger when event occurs
-var clickACard = function (){
-cardNumber.addEventListener("data-card", cardNumber[i]);
-
-	if (cardOne == cardTwo || cardThree == cardFour){
-	alert("Try again");
-	}
-	else if (cardOne == cardFour || cardTwo == cardThree){
-	alert("You found a match!");
 	}
 };
 
-//Adding event listener
-for (var i = 0; i < cardNumber.length; i++){
-gameBoard.addEventListener("click", clickACard);
-}
-isTwoCards =function () {
-	cardsInPlay.push(this.click("data-card"));
-		if (cardsInPlay.length ===2){
+//Createing isMatch to test if two cards in play are a match.
+var isMatch = function () {
+	 if (cardsInPlay[0] == cardsInPlay[1]){
+		 alert("You have a match!");
+	 } else {
+		 alert("You don't have a match");
+	 }
+	//  cards.innerHTML = "";
+};
+
+//declaring isTwoCards, accessing cards, cleararray
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		if (cardsInPlay.length === 2) {
 			isMatch(cardsInPlay);
 			cardsInPlay = [];
 		}
-}
+	};
+
+//display the gameBoard
+createBoard();
